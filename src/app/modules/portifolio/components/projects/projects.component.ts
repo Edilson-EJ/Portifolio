d27dce79fbbe4cbb3955654c0425aeadf6432764
Edilson-EJ <edilson.franca.melo@gmail.com>
2025-01-17 11:06:32 -0300

    criação do scss de projects e ajustes de ts.

--- src/app/modules/portifolio/components/projects/projects.component.ts
+++ src/app/modules/portifolio/components/projects/projects.component.ts
@@ -1,14 +1,19 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { IProjects } from '../../interface/IProjects.interface';
+import { MatDialogModule, MatDialog } from '@angular/material/dialog';
+import { EDialogPainelClass } from '../../enum/EDialogPainelClass.enum';
+import { DialogProjectsComponent } from '../dialog/dialog-projects/dialog-projects.component';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
 })
 export class ProjectsComponent {
+  #dialog = inject(MatDialog);
+
   public arrayProjects = signal<IProjects[]>([
     {
       src: 'assets/img/projects/android.svg',
@@ -25,4 +30,11 @@ export class ProjectsComponent {
       ],
     },
   ]);
+
+  public openDialog(data: IProjects) {
+    this.#dialog.open(DialogProjectsComponent, {
+      data,
+      panelClass: EDialogPainelClass.projects,
+    });
+  }
 }
---
+++ src/app/modules/portifolio/enum/EDialogPainelClass.enum.ts
@@ -0,0 +1,3 @@
+export enum EDialogPainelClass {
+  projects = 'dialog-container',
+}
